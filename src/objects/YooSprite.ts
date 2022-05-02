a0bda2d7f4a61f64ac2e6f7051191563cbe17aaa
Yong Hee <yyoo5@id.iit.edu>
2022-05-02 14:38:23 -0500

    clean up code - remove comments

--- src/objects/YooSprite.ts
+++ src/objects/YooSprite.ts
@@ -6,10 +6,6 @@ export default class YooSprite extends Phaser.Physics.Arcade.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'main-character');
 
-    // this.setScale(0.5);
-
-    // this.enemyName = enemyName;
-
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
@@ -38,23 +34,15 @@ export default class YooSprite extends Phaser.Physics.Arcade.Sprite {
       this.setVelocityX(0);
     }
 
-    // console.log(this.body.blocked.down);
-
     if (this.cursors.up.isDown && this.body.blocked.down) {
       this.setVelocityY(-240);
     }
-    // if (this.cursors.up.isDown) {
-    //   this.setVelocityY(-210);
-    // }
   }
 
   private makeAnimations() {
     this.anims.create({
       key: 'yoo-idle-right',
       frames: this.anims.generateFrameNames('main-character', {
-        // prefix: 'yoo-walk-right-',
-        // start: 1,
-        // end: 5,
         start: 7,
         end: 7,
       }),
