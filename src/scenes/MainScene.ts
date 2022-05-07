e86484e12c69dd949a46ffb3402cf0155c9c909c
Yong Hee <yyoo5@id.iit.edu>
2022-05-07 15:01:00 -0500

    clean up some code

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -63,24 +63,6 @@ export default class MainScene extends Phaser.Scene {
       this
     );
 
-    // const width = this.scale.width;
-    // const height = this.scale.height;
-
-    // //add fog of war
-    // const rt = this.make.renderTexture({ width, height }, true);
-    // rt.fill(0x000000, 1);
-
-    // this.vision = this.make.image({
-    //   x: this.yoo.x,
-    //   y: this.yoo.y,
-    //   key: 'vision',
-    //   add: false,
-    // });
-    // this.vision.scale = 1.5;
-
-    // rt.mask = new Phaser.Display.Masks.BitmapMask(this, this.vision);
-    // rt.mask.invertAlpha = true;
-
     //Add Camera and zoom
     this.cameras.main.startFollow(this.yoo, true);
     this.cameras.main.zoom = 2.0;
